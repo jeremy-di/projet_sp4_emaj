0996551d77a3c4367a34ce3fe0167001a4651584
jeremy-di <delta.i.developpement@gmail.com>
2026-05-26 14:05:53 +0200

    login - 2fa

--- back/controllers/user.controller.js
+++ back/controllers/user.controller.js
@@ -3,7 +3,7 @@ import User from "../models/user.model.js"
 import userValidation from "../validations/user.validation.js"
 import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
-import { authenticator } from "otplib"
+import { authenticator } from "@otplib/preset-default"
 import qrcode from "qrcode"
 
 const register = async(req,res)=>{
@@ -29,6 +29,66 @@ const register = async(req,res)=>{
     }
 }
 
+const generate2FA = async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id)
+
+        if (!user) {
+            return res.status(404).json({ msg : "Userr not found" })
+        }
+
+        const secret = authenticator.generateSecret()
+
+        const otpauth = authenticator.keyuri(
+            user.email,
+            "sp4_emaj",
+            secret
+        )
+
+        const qrcodeImage = await qrcode.toDataURL(otpauth)
+
+        user.twoFactorSecret = secret
+        await user.save()
+
+        return res.status(200).json({ msg: "QRCODE geneated", qrcodeImage })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ msg: "Server error", error: error })        
+    }
+}
+
+const enable2FA = async (req, res) => {
+    try {
+            const { token } = req.body
+            const user = await User.findById(req.user.id)
+
+            if (!user) {
+                return res.status(404).json({ msg : "Userr not found" })
+            }
+
+            if (!user.twoFactorSecret) {
+                return res.status(400).json({ message: "2FA secret not genrated" })
+            }
+
+            const isValid = authenticator.verify({
+                token,
+                secret: user.twoFactorSecret
+            })
+
+            if (!isValid) {
+                return res.status(400).json({ message: "Invalid 2FA code" })
+            }
+            
+            user.twoFactorEnabled = true
+            await user.save()
+
+            return res.status(200).json({ msg : "2FA enabled successfully" })
+        } catch (error) {
+            console.log(error)
+            return res.status(500).json({ msg: "Server error", error: error })
+        }
+}
+
 const login = async(req, res) => {
     try {
         const {email, password } = req.body
@@ -46,6 +106,18 @@ const login = async(req, res) => {
         if(!isMatch){
             return res.status(400).json({message: "invalid invalides"})
         }
+
+        if (user.isBlocked) {
+            return res.status(403).json({ msg: "Your account is blocked" })
+        }
+
+        if (user.twoFactorEnabled) {
+            const tempToken = jwt.sign({ id: user._id, email:  user.email, twoFactorPending: true }, process.env.SECRET_KEY, { expiresIn: "5m" })
+            
+            return res.status(200).json({ msg: "2FA required", twoFactorRequired: true, tempToken })
+        }
+
+
         res.status(200).json({
             message: user.email+" is connected",
             token: jwt.sign({ id: user._id, email:  user.email }, process.env.SECRET_KEY, { expiresIn: "12h" })
@@ -53,6 +125,50 @@ const login = async(req, res) => {
     } catch (error) {
         console.log(error)
         res.status(500).json({message: "Server error", error: error})
+    }
+}
+
+const verify2FA = async (req, res) => {
+    try {
+        const { token } = req.body
+
+        const authHeader = req.headers["authorization"]
+        const temptoken = authHeader && authHeader.split(" ")[1]
+
+        if (!temptoken) {
+            return res.status(401).json({ msg: "Missing tempory token" })
+        }
+
+        const decoded = jwt.verify(temptoken, process.env.SECRET_KEY)
+
+        if(!decoded.twoFactorPending) {
+            return res.status(403).json({ msg: "Invalid tempory token" })
+        }
+
+        const user = await User.findById(decoded.id)
+
+        if(!user) {
+            return res.status(404).json({ msg: "User not found" })
+        }
+
+        const isValid = authenticator.verify({
+            token,
+            secret: user.twoFactorSecret
+        })
+
+        if(!isValid) {
+            return res.status(400).json({ msg: "Invalid 2FA code" })
+        }
+
+        const finalToken = jwt.sign({ id: user._id, email:  user.email }, process.env.SECRET_KEY, { expiresIn: "12h" })
+
+        return res.status(200).json({
+            message: user.email+" is connected",
+            token: finalToken
+        })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ message: "Server error", error })
     }
 }
 
@@ -114,4 +230,4 @@ const deleteUser = async(req, res) => {
     }
 }
 
-export { register, login, getAllUsers, getUserById, updateUser, deleteUser }
+export { register, generate2FA, enable2FA, login, verify2FA, getAllUsers, getUserById, updateUser, deleteUser }
--- back/routes/user.route.js
+++ back/routes/user.route.js
@@ -1,14 +1,18 @@
 
 import { Router } from "express";
-import { register, login, getAllUsers, getUserById, updateUser, deleteUser } from "../controllers/user.controller.js"
+import { register, generate2FA, enable2FA, login, verify2FA, getAllUsers, getUserById, updateUser, deleteUser } from "../controllers/user.controller.js"
+import auth from '../middlewares/auth.js'
 
 const router = Router()
 
 router.post('/register', register)
 router.post('/login', login)
-router.get('/all', getAllUsers)
-router.get('/:id', getUserById)
-router.put('/:id', updateUser)
-router.delete('/:id', deleteUser)
+router.post('/login/2fa', verify2FA)
+router.post('/2fa/generate', auth, generate2FA)
+router.post('/2fa/enable', auth, enable2FA)
+router.get('/all', auth, getAllUsers)
+router.get('/:id', auth, getUserById)
+router.put('/:id', auth, updateUser)
+router.delete('/:id', auth, deleteUser)
 
 export default router
