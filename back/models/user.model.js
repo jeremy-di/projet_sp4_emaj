e5829ee6010b15670b545ed8231cc0cdbdb8fc37
jeremy-di <delta.i.developpement@gmail.com>
2026-05-26 14:36:51 +0200

    role user

--- back/models/user.model.js
+++ back/models/user.model.js
@@ -15,6 +15,11 @@ const userSchema = new mongoose.Schema({
       type: String,
       required: true
     },
+    role: {
+      type: String,
+      required: true,
+      default: "user"
+    },
     isBlocked: {
       type: Boolean,
       required: true,
